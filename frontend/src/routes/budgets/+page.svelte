19bf6a58919cbe1ff8ece6f51bbeda8cb32a20f4
Felix Rollbühler <xilef@MacBook-Air-von-Felix.local>
2022-12-29 08:41:59 +0100

    change budgets page to work with new backend

--- frontend/src/routes/budgets/+page.svelte
+++ frontend/src/routes/budgets/+page.svelte
@@ -1,33 +1,50 @@
 <script>
-	import { page } from '$app/stores';
 	import List from '../../lib/components/List.svelte';
 	import Chart from '../../lib/components/Chart.svelte';
+	import { onMount } from 'svelte';
+	import fetch from '../../lib/api/fetch'
 
-	export let data;
-	const budgets = data.budgets;
-	const inbudgets = data.inbudgets;
+	let budgets = null
+	let inbudgets = null
+    onMount(async () => {
+		const budgetsResult = (await (await fetch(`/api/history/budget?len=1&month=0&year=1`)).json()).map(item => {
+			item.date = `${new Date(item.date).getMonth()+1}-${new Date(item.date).getFullYear()}`;
+			return item
+		})[0]
+		budgets = Object.keys(budgetsResult)
+					.filter(name => name !== 'date')
+					.map(name => ({
+						name: name, 
+						value: budgetsResult[name].value, 
+						diff: budgetsResult[name].diff,
+					}))
+		const inbudgetsResult = (await (await fetch(`/api/history/inbudget?len=1&month=0&year=1`)).json()).map(item => {
+			item.date = `${new Date(item.date).getMonth()+1}-${new Date(item.date).getFullYear()}`;
+			return item
+		})[0]
+		inbudgets = Object.keys(inbudgetsResult)
+					.filter(name => name !== 'date')
+					.map(name => ({
+						name: name, 
+						value: inbudgetsResult[name].value, 
+						diff: inbudgetsResult[name].diff,
+					}))
+    })
+
 </script>
 
 <div class="main">
-	<h1>Quickselector</h1>
-	<ul>
-		<li><a href={$page.url.pathname}>All Time</a></li>
-		<li><a href="{$page.url.pathname}?startDate=2021&endDate=2022">2021</a></li>
-		<li><a href="{$page.url.pathname}?startDate=2022&endDate=2023">2022</a></li>
-		<li><a href="{$page.url.pathname}?startDate=2022-10&endDate=2022-11">2022 Oktober</a></li>
-		<li><a href="{$page.url.pathname}?startDate=2022-11&endDate=2022-12">2022 November</a></li>
-		<li><a href="{$page.url.pathname}?startDate=2022-12&endDate=2023">2022 Dezember</a></li>
-	</ul>
 	<h1>Out</h1>
+	{#if budgets}
 	<div class="chart-wrapper">
 		<Chart
 			type="doughnut"
 			data={{
-				labels: budgets.map((inbudget) => inbudget.name),
+				labels: budgets.map(item => item.name),
 				datasets: [
 					{
 						label: 'Sum',
-						data: budgets.map((inbudget) => inbudget.amount / 100),
+						data: budgets.map(item => item.value / 100),
 						borderWidth: 1
 					}
 				],
@@ -36,28 +53,30 @@
 		/>
 	</div>
 	<List
-		items={budgets.map((budget) => ({
-			title: budget.name,
+		items={budgets.map((item) => ({
+			title: item.name,
 			subtitle: null,
 			subtitleIcon: null,
-			amount: budget.amount,
-			color: budget.amount < 0 ? 'green' : budget.amount > 0 ? 'red' : 'gray',
+			amount: item.value,
+			color: item.value < 0 ? 'green' : item.value > 0 ? 'red' : 'gray',
 			link: null,
-			link2: `/transactions?budget=${budget.name}`,
+			link2: `/transactions?budget=${item.value}`,
 			link3: null,
 			newBlock: null
 		}))}
 	/>
+	{/if}
 	<h1>In</h1>
+	{#if inbudgets}
 	<div class="chart-wrapper">
 		<Chart
 			type="doughnut"
 			data={{
-				labels: inbudgets.map((inbudget) => inbudget.name),
+				labels: inbudgets.map((item) => item.name),
 				datasets: [
 					{
 						label: 'Sum',
-						data: inbudgets.map((inbudget) => inbudget.amount / 100),
+						data: inbudgets.map((item) => item.value / 100),
 						borderWidth: 1
 					}
 				],
@@ -66,18 +85,19 @@
 		/>
 	</div>
 	<List
-		items={inbudgets.map((inbudget) => ({
-			title: inbudget.name,
+		items={inbudgets.map((item) => ({
+			title: item.name,
 			subtitle: null,
 			subtitleIcon: null,
-			amount: inbudget.amount,
-			color: inbudget.amount > 0 ? 'green' : inbudget.amount < 0 ? 'red' : 'gray',
+			amount: item.value,
+			color: item.value > 0 ? 'green' : item.value < 0 ? 'red' : 'gray',
 			link: null,
-			link2: `/transactions?inbudget=${inbudget.name}`,
+			link2: `/transactions?inbudget=${item.name}`,
 			link3: null,
 			newBlock: null
 		}))}
 	/>
+	{/if}
 </div>
 
 <style>

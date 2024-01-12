df442e98c16be3b16eecf3b37fb21f98c5c71314
Felix Rollbühler <xilef@MacBook-Air-von-Felix.local>
2024-01-12 18:00:39 +0100

    use fancy_yew resource in goals page

--- frontend/src/pages/goals.rs
+++ frontend/src/pages/goals.rs
@@ -1,74 +1,23 @@
 use crate::tmp_fancy_yew::ListItem;
 use crate::Route;
+use fancy_yew::rest::Resource;
 use money_app_shared::goal::{Goal, GoalData};
 
-use gloo::net::http::Request;
 use stylist::Style;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
 #[function_component]
 pub fn Goals() -> Html {
-    let goals = use_state(|| vec![]);
-    let load_goals = {
-        let goals = goals.clone();
-        move || {
-            let goals = goals.clone();
-            wasm_bindgen_futures::spawn_local(async move {
-                let fetched_goals: Vec<Goal> = Request::get("/api/goals")
-                    .send()
-                    .await
-                    .unwrap()
-                    .json()
-                    .await
-                    .unwrap();
-                goals.set(fetched_goals);
-            });
-        }
-    };
+    let goals: UseStateHandle<Resource<Goal>> =
+        use_state(|| Resource::new("/api/goals".into(), Vec::default()));
     {
-        let load_goals = load_goals.clone();
+        let goals = goals.clone();
         use_effect_with((), move |_| {
-            load_goals();
+            Resource::reload(goals);
             || ()
         });
     }
-
-    let add = {
-        let load_goals = load_goals.clone();
-        move |_: MouseEvent| {
-            let goals = vec![Goal::default()];
-            let load_goals = load_goals.clone();
-            wasm_bindgen_futures::spawn_local(async move {
-                Request::post("/api/goals")
-                    .json(&goals)
-                    .unwrap()
-                    .send()
-                    .await
-                    .unwrap();
-                load_goals();
-            });
-        }
-    };
-
-    let delete = {
-        let load_goals = load_goals.clone();
-        move |id: String| {
-            let mut goal = Goal::default();
-            goal.id = Some(id);
-            let goals = vec![goal];
-            let load_goals = load_goals.clone();
-            wasm_bindgen_futures::spawn_local(async move {
-                Request::delete("/api/goals")
-                    .json(&goals)
-                    .unwrap()
-                    .send()
-                    .await
-                    .unwrap();
-                load_goals();
-            });
-        }
-    };
 
     let navigator = use_navigator().unwrap();
     let goal_items = goals
@@ -83,14 +32,12 @@ pub fn Goals() -> Html {
                         navigator.push(&Route::Goal { id: id.clone() })
                     })
                 };
-                let delete = delete.clone();
-                let ondelete = Callback::from(move |_: MouseEvent| delete(id.clone()));
                 html! {<ListItem
                     title={"Wealth"}
                     subtitle={goal.due.format("%d %b %Y").to_string()}
                     amount={wealth as i64}
                     onedit={onedit}
-                    ondelete={ondelete}
+                    ondelete={Resource::delete_callback(goal.clone(),goals.clone())}
                 />}
             }
         })
@@ -101,7 +48,7 @@ pub fn Goals() -> Html {
             <div class="header">
                 <button
                     class="material-symbols-outlined icon"
-                    onclick={add}
+                    onclick={Resource::add_default_closure::<MouseEvent>(goals.clone())}
                 >{"add"}</button>
             </div>
             <ul>
